8f0d9affd030083b5b0c22d9460e0ba843b75bc1
Ivan Silvosa Rodriguez <ivansrlsy@gmail.com>
2024-09-26 14:08:39 +0200

    Ejercicio Resuelto

--- src/main/java/com/campusdual/classroom/Doctor.java
+++ src/main/java/com/campusdual/classroom/Doctor.java
@@ -8,4 +8,18 @@ public class Doctor extends Person {
 		super(name, surname);
 		this.specialization = specialization;
 	}
+
+	public void getDetails(){
+		System.out.println("El nombre del doctor es:"+ name);
+		System.out.println("El apellido del doctor es:"+ surname);
+		System.out.println("La especialización del docotr es:"+specialization);
+	}
+
+	public String getSpecialization() {
+		return specialization;
+	}
+
+	public void setSpecialization(String specialization) {
+		this.specialization = specialization;
+	}
 }
--- src/main/java/com/campusdual/classroom/Person.java
+++ src/main/java/com/campusdual/classroom/Person.java
@@ -8,4 +8,28 @@ public class Person {
 		this.name = name;
 		this.surname = surname;
 	}
+
+	public Person() {
+	}
+
+	public void getDetails(){
+		System.out.println("Aqui se muestra el nombre: "+ name);
+		System.out.println("Aqui se mostrará el apellido"+ surname);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
 }
